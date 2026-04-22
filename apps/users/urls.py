420b2ec7127ba688bc48901aea60e0372ff81284
Parinat Kanyala <parinatk1@gmail.com>
2026-04-22 23:57:32 +0700

    add sign up

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,8 +6,8 @@ app_name = 'users'  # ← namespace, important
 
 urlpatterns = [
     # path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
+    path('signin/', views.signin_view, name='signin'),
+    path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('forgot/', views.reset_password_view, name='forgot'),
     # path('profile/', views.profile_view, name='profile'),
--- apps/users/views.py
+++ apps/users/views.py
@@ -3,9 +3,10 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .forms import SignUpForm
 
 # Create your views here.
-def login_view(request):
+def signin_view(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -13,27 +14,34 @@ def login_view(request):
         # Check if user provide not exists email
         if not User.objects.filter(username=username).exists():
             messages.error(request, 'Invalid username')
-            return redirect('/users/login/')
+            return redirect('/users/signin/')
 
         user = authenticate(username=username, password=password)
 
         if user is None:
             #Display error message password incorrect
             messages.error(request,'Invalid password')
-            return redirect('/users/login/')
+            return redirect('/users/signin/')
         else:
             login(request,user)
-            redirect('/')
+            return redirect('/')
 
-    return render(request, 'users/login.html')
+    return render(request, 'users/signin.html')
 
 def logout_view(request):
     logout(request)
     messages.success(request,'Logout Success')
-    return redirect('/users/login/')
+    return redirect('/users/signin/')
 
-def register_view(request):
-    pass
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/users/signin/')
+    else:
+        form = SignUpForm()
+    return render(request, 'users/signup.html',{'form':form})
 
 def reset_password_view(request):
     pass
